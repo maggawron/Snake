72579a3e9cd92623b94beeed6779bf5cf3200d42
Magdalena Gawron <magdalena.gawron.bb@gmail.com>
2020-06-14 14:57:39 +0200

    Snake moving on key press

--- Screen.py
+++ Screen.py
@@ -7,28 +7,23 @@ class Screen:
         apple_sign = "O"
         snake_sign = "S"
 
+        snake_loc_set = set(snake_loc)
+        obstacle_loc_set = set(obstacle_loc)
+        apple_loc_set = set(apple_loc)
+        #print(snake_loc_set, obstacle_loc_set, apple_loc_set)
+
         for row_id in range(length):
             for col_id in range(width):
                 if row_id == 0 or col_id == 0 or row_id == length - 1 or col_id == width - 1:
                     print(border_sign, end="")
-                elif (row_id, col_id) in obstacle_loc:
-                    print(obstacle_sign, end="")
-                elif (row_id, col_id) in apple_loc:
-                    print(apple_sign, end="")
-                elif (row_id, col_id) in snake_loc:
+                elif (row_id, col_id) in snake_loc_set:
                     print(snake_sign, end="")
+                elif (row_id, col_id) in obstacle_loc_set:
+                    print(obstacle_sign, end="")
+                elif (row_id, col_id) in apple_loc_set:
+                    print(apple_sign, end="")
                 else:
                     print(" ", end="")
                 #End a line
                 if col_id == width - 1:
                     print("")
-
-display = Screen()
-width = 50
-length = 10
-obstacle_loc = {(2,7), (4,5)}
-apple_loc = {(7,9)}
-snake_loc = {(5,6), (6,6), (7,6), (8,6), (8,7), (8,8)}
-
-display.print_screen(width, length, obstacle_loc, apple_loc, snake_loc)
-
--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import keyboard
+from Screen import Screen
+from Backend import Backend
+#from timer import Timer
 
-def pressed(event):
-    print(event.name)
+#def pressed(event):
+#    print(event.name)
 
 def main():
-    keyboard.on_press(pressed)
+    #t = Timer()
 
-    while True:
+    #setup game parameters
+    display = Screen()
+    b = Backend()
+    width = 50
+    length = 10
+    prev_key = None
+    apple_loc = []
+    obs_number = 3
+    apple_number = 2
+    obstacle_loc = b.generate_obstacle(width, length, obs_number)
+    snake_loc = b.generate_first_snake(obstacle_loc, width, length)
+    #print(snake_loc)
+    for _ in range(apple_number):
+        apple_loc = b.generate_apple(snake_loc, obstacle_loc, apple_loc, width, length)
+
+    #initiate intro screen of the game
+    display.print_screen(width, length, obstacle_loc, apple_loc, snake_loc)
+
+    #keyboard.on_press(pressed)
+
+    while b.check_if_snake_lives(snake_loc, obstacle_loc, width, length):
         key = keyboard.read_key()
+        snake_loc = b.update_snake(key, prev_key, snake_loc, apple_loc)
+        apple_eaten = 0
 
+        for apple in apple_loc:
+            if b.check_if_apple_eaten(snake_loc, apple_loc):
+                apple_loc.remove(apple)
+                apple_loc = b.generate_apple(snake_loc, obstacle_loc, apple_loc, width, length)
+
+        b.cls()
+        display.print_screen(width, length, obstacle_loc, apple_loc, snake_loc)
 
 # check
 if __name__ == "__main__":
